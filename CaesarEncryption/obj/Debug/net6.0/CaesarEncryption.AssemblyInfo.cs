7e548e3c195e1858ee16958b0890be6eefa50e56
Michał Niekrasz <mnierasz@gmail.com>
2022-06-16 19:36:15 +0200

    metadata changes

--- CaesarEncryption/obj/Debug/net6.0/CaesarEncryption.AssemblyInfo.cs
+++ CaesarEncryption/obj/Debug/net6.0/CaesarEncryption.AssemblyInfo.cs
@@ -12,12 +12,12 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Michal Niekrasz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute(@"encrypt function (string, key1, key2), the result of which is the encrypted text with a cipher Caesar with modification - the vowels were coded with key1, and the consonants were coded with key2. decrypt function (string, mostcommon) - result is decrypted text from a chipher Caesar. Note, that text has to be long enough for the program to find the most common letter. Use only characters that are between 127 and 32 (including) from ASCII table.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute(@"(bug free version). encrypt function (string, key1, key2), the result of which is the encrypted text with a cipher Caesar with modification - the vowels were coded with key1, and the consonants were coded with key2 (key2 dont have to be set). decrypt function (string, mostcommon) - result is decrypted text from a chipher Caesar. Note, that text has to be long enough for the program to find the most common letter. Use only characters that are between 127 and 32 (including) from ASCII table. if you detect any bugs, please, contact me")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaesarEncryption")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaesarEncryption")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/MichalNiekrasz/CaesarEncryption.git")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
